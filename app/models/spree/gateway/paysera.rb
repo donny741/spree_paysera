5c02249d3313e1affffd14511d84bd5f251bc8c9
donny741 <ddonatasjar@gmail.com>
2019-12-20 20:59:04 +0200

    rubocop styling fixes

--- app/models/spree/gateway/paysera.rb
+++ app/models/spree/gateway/paysera.rb
@@ -1,28 +1,31 @@
+# frozen_string_literal: true
+
 class Spree::Gateway::Paysera < Spree::Gateway
-    preference :project_id, :integer
-    preference :sign_key, :string
-    preference :api_version, :string, default: '1.6'
-    preference :domain_name, :string
-    preference :message_text, :string
-    preference :service_url, :string, default: 'https://www.paysera.lt/pay/?'
-    preference :image_url, :string, default: 'https://bank.paysera.com/assets/image/payment_types/wallet.png'
+  preference :project_id, :integer
+  preference :sign_key, :string
+  preference :api_version, :string, default: '1.6'
+  preference :domain_name, :string
+  preference :message_text, :string
+  preference :service_url, :string, default: 'https://www.paysera.lt/pay/?'
+  preference :image_url, :string, default: 'https://bank.paysera.com/assets/image/payment_types/wallet.png'
 
-    def provider_class
-      Paysera
-    end
-    def source_required?
-        false
-    end
-    def auto_capture?
-        true
-    end
-  
-    def method_type
-      'paysera'
-    end
-
-    def purchase(amount, transaction_details, options = {})
-        ActiveMerchant::Billing::Response.new(true, 'Paysera success', {},{})
-    end
-    
+  def provider_class
+    Paysera
   end
+
+  def source_required?
+    false
+  end
+
+  def auto_capture?
+    true
+  end
+
+  def method_type
+    'paysera'
+  end
+
+  def purchase(_amount, _transaction_details, _options = {})
+    ActiveMerchant::Billing::Response.new(true, 'Paysera success', {}, {})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Spree::Core::Engine.add_routes do
-  post '/paysera', :to => "paysera#index", :as => :paysera_proceed
-  get '/paysera/:payment_method_id/callback', :to => "paysera#callback", :as => :paysera_callback
-  get '/paysera/:payment_method_id/confirm', :to => "paysera#confirm", :as => :paysera_confirm
-  get '/paysera/:payment_method_id/cancel', :to => "paysera#cancel", :as => :paysera_cancel
+  post '/paysera', to: 'paysera#index', as: :paysera_proceed
+  get '/paysera/:payment_method_id/callback', to: 'paysera#callback', as: :paysera_callback
+  get '/paysera/:payment_method_id/confirm', to: 'paysera#confirm', as: :paysera_confirm
+  get '/paysera/:payment_method_id/cancel', to: 'paysera#cancel', as: :paysera_cancel
 end
